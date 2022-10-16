bb85ea9adde37f3945a43732999fbfaa82582b8e
Isma3elovic <110466601+Isma3elovic@users.noreply.github.com>
2022-10-16 11:48:25 +0200

    Add files via upload

--- assignment2/src/main/java/assignment2/assignment2/Account/AccountController.java
+++ assignment2/src/main/java/assignment2/assignment2/Account/AccountController.java
@@ -2,6 +2,7 @@ package assignment2.assignment2.Account;
 
 import assignment2.assignment2.Dto;
 import assignment2.assignment2.Request;
+import assignment2.assignment2.Response;
 import assignment2.assignment2.exception.ApiRequestException;
 import assignment2.assignment2.statement.StatementEntity;
 import assignment2.assignment2.statement.StatmentRepo;
@@ -9,6 +10,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,83 +38,84 @@ public class AccountController {
     Dto dto = new Dto();
 
     @PostMapping("/all")
-    public ResponseEntity<Object> findById(@RequestBody String json) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        Request obj = mapper.readValue(json, Request.class);
-        System.out.println(obj.getId());
-        return null;
-        // Optional<AccountEntity> accountEntity = accountRepo.findById(id);
+    public ResponseEntity<Object> findById(@RequestBody Request req )  {
+        Response res = new Response();
+        BeanUtils.copyProperties(req,res);
+        System.out.println(res.getId());
+       // return ResponseEntity.ok(res);
 
-//        System.out.println(from);
+       //  Optional<AccountEntity> accountEntity = accountRepo.findById(res.getId());
+
+        //System.out.println(res.getFrom());
+
+        Optional<AccountEntity> accountEntity = accountRepo.findAccountEntityById(res.getId());
+        if (accountEntity.isPresent()) {
+            System.out.println();
+            if (res.getFrom() != null && res.getTo() != null) {
+                //     System.out.println("hello");
+                try {
+                    Date fromTime = new SimpleDateFormat("dd.MM.yyyy").parse(res.getFrom());
+                    System.out.println(fromTime);
+                    Date toTime = new SimpleDateFormat("dd.MM.yyyy").parse(res.getTo());
+                    System.out.println(toTime);
+                    if (fromTime.before(toTime)) {
+                        List<StatementEntity> statementEntities = accountRepo.findAccountEntityById(res.getId()).get().getStatementEntity();
+
+                        List<StatementEntity> statementEntitiesFilterd = statementEntities.stream().filter(new Predicate<StatementEntity>() {
+                            @Override
+                            public boolean test(StatementEntity statementEntity) {
+                                try {
+                                    Date statementEntityDate = new SimpleDateFormat("dd.MM.yyyy").parse(statementEntity.getDate());
 //
-//        Optional<AccountEntity> accountEntity = accountRepo.findAccountEntityById(id);
-//        if (accountEntity.isPresent()) {
-//            System.out.println();
-//            if (from.isPresent() && to.isPresent()) {
-//                //     System.out.println("hello");
-//                try {
-//                    Date fromTime = new SimpleDateFormat("dd.MM.yyyy").parse(from.get());
-//                    System.out.println(fromTime);
-//                    Date toTime = new SimpleDateFormat("dd.MM.yyyy").parse(to.get());
-//                    System.out.println(toTime);
-//                    if (fromTime.before(toTime)) {
-//                        List<StatementEntity> statementEntities = accountRepo.findAccountEntityById(id).get().getStatementEntity();
-//
-//                        List<StatementEntity> statementEntitiesFilterd = statementEntities.stream().filter(new Predicate<StatementEntity>() {
-//                            @Override
-//                            public boolean test(StatementEntity statementEntity) {
-//                                try {
-//                                    Date statementEntityDate = new SimpleDateFormat("dd.MM.yyyy").parse(statementEntity.getDate());
-////
-////                                    return statementEntityDate.after(fromTime) && statementEntityDate.before(toTime);
-//
-//
-//                                    if ((statementEntityDate.equals(fromTime) || statementEntityDate.equals(toTime)))
-//                                        return statementEntityDate.equals(fromTime) || statementEntityDate.equals(toTime);
-//
-//                                    else if (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime))
-//                                        return (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime));
-////                                ( (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime)));
-//                                    else
-//                                        return false;
-//
-//                                } catch (Exception e) {
-//                                    throw new ApiRequestException("invalid date");
-//                                }
-//                            }
-//                        }).collect(Collectors.toList());
-//
-//                       // System.out.println(String.valueOf(statementEntitiesFilterd.get(0).getId()));
-//
-//                        return ResponseHandler.generateResponse(accountEntity,statementEntitiesFilterd,HttpStatus.OK, false, "Success", null);
-//
-//
-//                        //  return new ResponseEntity<>(statementEntitiesFilterd, HttpStatus.OK);
-//
-//                    } else {
-//                        throw new ApiRequestException("from date should be before ");
-//                    }
-//
-//                } catch (Exception exception) {
-//
-//                    throw new ApiRequestException("one of dates are not valid");
-//                }
-//            } else {
-//                List<StatementEntity> statementEntities = accountRepo.findAccountEntityById(id).get().getStatementEntity();
-//
-//
-//                //List<StatementEntity> statementEntitiesInThreeMonth = statmentRepo.findStatementEntitiesByCreatedAtBetweenAndAccountEntity_Id(Date.from(LocalDateTime.now().minusMonths(3).toInstant(ZoneOffset.UTC)), Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)), id);
-//                //return new ResponseEntity<>(statementEntitiesInThreeMonth, HttpStatus.OK);
-//
-//                return ResponseHandler.generateResponse(accountEntity,statementEntities,HttpStatus.OK, false, "Success", null);
-//
-//            //    return new ResponseEntity<>(statementEntities, HttpStatus.OK);
-//            }
-//        } else {
-//            throw new ApiRequestException("account not found");
-//
-//        }
-//    }
+//                                    return statementEntityDate.after(fromTime) && statementEntityDate.before(toTime);
+
+
+                                    if ((statementEntityDate.equals(fromTime) || statementEntityDate.equals(toTime)))
+                                        return statementEntityDate.equals(fromTime) || statementEntityDate.equals(toTime);
+
+                                    else if (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime))
+                                        return (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime));
+//                                ( (statementEntityDate.after(fromTime) && statementEntityDate.before(toTime)));
+                                    else
+                                        return false;
+
+                                } catch (Exception e) {
+                                    throw new ApiRequestException("invalid date");
+                                }
+                            }
+                        }).collect(Collectors.toList());
+
+                       // System.out.println(String.valueOf(statementEntitiesFilterd.get(0).getId()));
+
+                        return ResponseHandler.generateResponse(accountEntity,statementEntitiesFilterd,HttpStatus.OK, false, "Success", null);
+
+
+                        //  return new ResponseEntity<>(statementEntitiesFilterd, HttpStatus.OK);
+
+                    } else {
+                        throw new ApiRequestException("from date should be before ");
+                    }
+
+                } catch (Exception exception) {
+
+                    throw new ApiRequestException("one of dates are not valid");
+                }
+            } else {
+                List<StatementEntity> statementEntities = accountRepo.findAccountEntityById(res.getId()).get().getStatementEntity();
+
+
+                //List<StatementEntity> statementEntitiesInThreeMonth = statmentRepo.findStatementEntitiesByCreatedAtBetweenAndAccountEntity_Id(Date.from(LocalDateTime.now().minusMonths(3).toInstant(ZoneOffset.UTC)), Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)), id);
+                //return new ResponseEntity<>(statementEntitiesInThreeMonth, HttpStatus.OK);
+
+                return ResponseHandler.generateResponse(accountEntity,statementEntities,HttpStatus.OK, false, "Success", null);
+
+            //    return new ResponseEntity<>(statementEntities, HttpStatus.OK);
+            }
+        } else {
+            throw new ApiRequestException("account not found");
+
+        }
     }
-}
+    }
+
 
--- assignment2/src/main/java/assignment2/assignment2/Request.java
+++ assignment2/src/main/java/assignment2/assignment2/Request.java
@@ -2,10 +2,11 @@ package assignment2.assignment2;
 
 public class Request {
     private long id;
-    private String fromAmount;
-    private String toAmount;
     private String from;
     private String to;
+//    private String fromAmount;
+//    private String toAmount;
+
 
     public long getId() {
         return id;
@@ -15,21 +16,21 @@ public class Request {
         this.id = id;
     }
 
-    public String getFromAmount() {
-        return fromAmount;
-    }
-
-    public void setFromAmount(String fromAmount) {
-        this.fromAmount = fromAmount;
-    }
-
-    public String getToAmount() {
-        return toAmount;
-    }
-
-    public void setToAmount(String toAmount) {
-        this.toAmount = toAmount;
-    }
+//    public String getFromAmount() {
+//        return fromAmount;
+//    }
+//
+//    public void setFromAmount(String fromAmount) {
+//        this.fromAmount = fromAmount;
+//    }
+//
+//    public String getToAmount() {
+//        return toAmount;
+//    }
+//
+//    public void setToAmount(String toAmount) {
+//        this.toAmount = toAmount;
+//    }
 
     public String getFrom() {
         return from;
